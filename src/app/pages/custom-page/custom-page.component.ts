cb73ce8c46acb69f5d77e2b5bd42fd0b715b1d38
FelipeVel <fvelandia695@gmail.com>
2025-06-06 21:55:09 -0500

    Agregar implementación de héroes y pipes personalizados para la gestión de datos de héroes

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
+import { CanFlyPipe } from '../../pipes/can-fly.pipe';
+import { ColorPipe } from '../../pipes/color.pipe';
+import { TextColorPipe } from '../../pipes/text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { CreatorPipe } from '../../pipes/creator.pipe';
+import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
-  imports: [],
+  imports: [
+    ToggleCasePipe,
+    CanFlyPipe,
+    ColorPipe,
+    TextColorPipe,
+    TitleCasePipe,
+    CreatorPipe,
+    HeroSortByPipe,
+  ],
   templateUrl: './custom-page.component.html',
 })
-export default class CustomPageComponent { }
+export default class CustomPageComponent {
+  name = signal('Felipe Velandia')
+
+  isUpper = signal(true);
+  toggleCase() {
+    this.isUpper.update(isUpper => !isUpper);
+  }
+
+  heroes = signal(heroes);
+  sortBy = signal<keyof Hero | null>(null);
+}
